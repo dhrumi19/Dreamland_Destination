808ca2398b62fc71c483327059f0734515eb9950
dhrumi19 <dhrumi.mscit24@skipsuniversity.edu.in>
2026-06-11 16:54:03 +0530

    Update ASGI config for dreamland_destination project

--- dreamland_destination/dreamland_destination/asgi.py
+++ dreamland_destination/dreamland_destination/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dreamland_destination.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dreamland_destination.dreamland_destination.settings')
 
 application = get_asgi_application()
